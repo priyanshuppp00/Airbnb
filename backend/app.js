6f1ff2178f3c5e7489a934530161fe29282e76e4
Priyanshu Kumar <priyanshucoolpks@gmail.com>
2025-09-14 16:21:39 +0530

    Update

--- backend/app.js
+++ backend/app.js
@@ -80,7 +80,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files
 app.use(express.static(path.join(rootDir, "public")));
